c6276e20ad4f20e007104ada93c5377d27317b02
MShahzodbek <mshahzodbek0309@gmail.com>
2023-07-14 16:45:39 +0500

    Initializing project files

---
+++ src/test/java/com/example/springsimpleproject/SpringSimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsimpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
